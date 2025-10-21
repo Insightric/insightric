4f0bfa5fe04b354779aa562c14e891f79027d722
Abdourahamane Issaka Sani <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2025-10-21 18:03:05 +0200

    Comment out Kubernetes config map resource for aws_auth to prevent accidental modifications

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -77,41 +77,40 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
-resource "kubernetes_config_map" "aws_auth" {
-  depends_on = [
-    aws_eks_cluster.insightric_cluster,
-    aws_eks_node_group.default
-  ]
+# resource "kubernetes_config_map" "aws_auth" {
+#   depends_on = [
+#     aws_eks_cluster.insightric_cluster,
+#     aws_eks_node_group.default
+#   ]
 
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+#   metadata {
+#     name      = "aws-auth"
+#     namespace = "kube-system"
+#   }
 
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = aws_iam_role.eks_node_role.arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
-      },
-      {
-        rolearn  = "arn:aws:iam::136086620476:role/insightric-gha-deployer"
-        username = "github-actions"
-        groups   = ["system:masters"]
-      }
-    ])
-  }
+#   data = {
+#     mapRoles = yamlencode([
+#       {
+#         rolearn  = aws_iam_role.eks_node_role.arn
+#         username = "system:node:{{EC2PrivateDNSName}}"
+#         groups = [
+#           "system:bootstrappers",
+#           "system:nodes"
+#         ]
+#       },
+#       {
+#         rolearn  = "arn:aws:iam::136086620476:role/insightric-gha-deployer"
+#         username = "github-actions"
+#         groups   = ["system:masters"]
+#       }
+#     ])
+#   }
 
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = [data]
-  }
-}
-
+#   lifecycle {
+#     prevent_destroy = false
+#     ignore_changes  = [data]
+#   }
+# }
 
 # EKS Cluster
 resource "aws_eks_cluster" "insightric_cluster" {
